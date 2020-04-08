578d9890a655f0ac27e49a6825f9e306592b2794
Aderson Neto <afcneto@outlook.com>
2020-04-08 20:07:19 -0300

    ajustes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
-import { Router } from 'express';
+/* import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello' });
+    const user = await User.create({
+        name: 'Aderson Neto',
+        email: 'afcneto@outlook.com',
+        password_hash: '123456',
+    });
+
+    return res.json(user);
 });
-module.exports = routes;
+module.exports = routes; */
